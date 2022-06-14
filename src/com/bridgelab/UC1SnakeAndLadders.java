3f1abc9fbba85ddb37245be61f69fac1b7ba63be
Shashank10101010 <shashankpraveen23@gmail.com>
2022-06-14 15:51:18 +0530

    UC1

---
+++ src/com/bridgelab/UC1SnakeAndLadders.java
@@ -0,0 +1,8 @@
+package com.bridgelab;
+
+public class UC1SnakeAndLadders {
+    public static void main(String[] args) {
+        int startPosition = 0;
+        System.out.print(startPosition);
+    }
+}
